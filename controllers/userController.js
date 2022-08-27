0519750fc94fbb58f7cb950349fcc5da9442b889
Rachit Rawat <rachitthakur311@gmail.com>
2022-08-27 16:36:46 +0530

    add logics for user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,58 @@ const userRegister = (req, res) =>{
             userEmail: req.body.userEmail,
             password: hash
         })
-        admin.save
+        admin.save()
+        .then (result=>{
+            res.status(200).json({
+                Data:result
+            })
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
     }
+    })
+}
+
+
+const userLogin = (res, req)=>{
+    user.find({userEmail: req.body.user})
+    .exe()
+    .then(user=>{
+        if(user.length<1)
+        {
+            return res.status(401).json({
+                msg: 'user not found'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+            if(!result){
+                return res.status(401).json({
+                    msg: 'password does not match'
+                })
+            }
+           if(result) 
+           {
+            const token = jwt.sign({
+                userEmail: user[0].userEmail
+            }, 
+            'this is dummy text',
+            {
+                expiresIn: "24h"
+            }
+            );
+            res.status(200).json({
+                userEmail: user[0].userEmail,
+                token: token
+            })
+           }
+        })
+    })
+    .catch(err=>{
+        res.status(500).json
+        error: err
     })
 }
