131199c25f89dace4f8912f6b62776372657541a
Rachit Rawat <rachitthakur311@gmail.com>
2022-09-02 10:36:42 +0530

    add functionality in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -95,8 +95,84 @@ const getUserProfile = (req,res,next)=>{
 const userChangePassword = async(req,res)=>{
 console.log('change password');
 try{
-    
+    const {userId} = req.params;
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const userPassword = await Users.findByIdAndUpdte({_Id: userId}, {password: password}, {new: true});
+    return res.status(200).json({
+        status: false,
+        data: userPassword
+    })
+}catch(err){
+    return res.status(400).json({
+        status: false,
+        error: 'Error Occured'
+    })
 }
 }
 
-module.exports ={userRegister, userLogin, getUserProfile }
+const addQuote = (req, res)=>{
+    const quote = new addQuote({
+        name: req.body.name,
+        emil: req.body.email,
+        mobile:req.mobile.email,
+        Requirements: req.body.Requirements,
+        message: req.body.message,
+    })
+    quote.save()
+    .then(result =>{
+        res.status(200).json({
+            result: result
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error: err,
+        })
+    })
+}
+
+const countryCode = (req,res)=>{
+    const countary = new country({
+        name: req.body.name,
+        dail_code: req.body.dail_code,
+        code: req.body.code
+    })
+    country.sav()
+    .then(result =>{
+        res.status(200).json({
+            result: result,
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error:err,
+        })
+    })
+}
+
+getCountry = (req, res)=>{
+    country.find()
+    .then(result=>{
+        res.status(200).json({
+            result: result
+        })
+    })
+    .catch( err =>{
+        res.status(500).json({
+            error:err,
+        })
+    })
+}
+
+updateCountry = (req, res)=>{
+    
+}
+
+
+
+
+
+
+
+module.exports ={userRegister, userLogin, getUserProfile, userChangePassword, getCountry}
